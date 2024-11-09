8b1c0e238f2ff572d4c7c6af678269294e0b07a9
Titipakorn <titipakorn.p@gmail.com>
2024-11-09 15:33:31 +0700

    Initial commit.

---
+++ src/onchain_oracle_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { onchain_oracle_backend } from 'declarations/onchain_oracle_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    onchain_oracle_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
